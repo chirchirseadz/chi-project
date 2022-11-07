ae3555bb93c6bf61b2e59491a58e3b95b9ada13e
kipchi <sydneychirchir@gmail.com>
2022-11-07 16:48:26 +0300

    Last updates nov 7

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv('SECRET_KEY')
  
-SECRET_KEY = config('SECRET_KEY') 
+SECRET_KEY=config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -164,6 +164,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Email settings
 
+
+
 EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
@@ -171,11 +173,3 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-
-
-# EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'your mail server you want '
-# EMAIL_PORT=587
-# EMAIL_USE_TLS=True
-# EMAIL_HOST_USER='email address in mail server e.g info@kenyaservices.com '
-# EMAIL_HOST_PASSWORD = ' your pass '
